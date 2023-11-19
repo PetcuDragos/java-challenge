e0fcf0bfe0c9040980a17bc786de84c5320d6550
Petcu Dragos <dyenigma@gmail.com>
2023-11-19 22:09:51 +0200

    Created the structure with spring initializer

---
+++ src/test/java/ro/codecrafters/BankingSystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.codecrafters.BankingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
